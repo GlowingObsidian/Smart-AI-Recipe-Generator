3254b03506e2ce1b1545bf7e59dcbc3cfa29da45
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-12-13 21:27:27 +0530

    Implemented user authentication

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,33 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <section className="bg-rose-200 relative flex flex-wrap lg:h-screen lg:items-center">
+      <div className="w-full px-4 sm:px-6 my-5 lg:w-1/2 lg:px-8 relative z-10">
+        <div className="mx-auto max-w-lg text-center">
+          <h1 className="text-2xl font-bold sm:text-3xl text-gray-900">
+            Get started today!
+          </h1>
+
+          <p className="mt-2 text-gray-500">
+            Simple and fun, evoking the idea of serving food via AI.
+          </p>
+        </div>
+        <div className="flex justify-center mt-5">
+          <SignIn />
+        </div>
+      </div>
+
+      <div className="absolute inset-0 lg:relative lg:h-full lg:w-1/2 lg:block">
+        <Image
+          alt=""
+          src="/auth_image.jpg"
+          className="h-full w-full object-cover"
+          width={1000}
+          height={800}
+        />
+      </div>
+    </section>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Smart AI Recipe Generator",
@@ -15,8 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ClerkProvider>{children}</ClerkProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
