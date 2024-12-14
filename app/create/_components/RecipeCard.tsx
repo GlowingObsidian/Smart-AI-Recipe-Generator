6bcffc4447d9e7610bb64da4db316fd9fb600781
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2024-12-14 13:00:18 +0530

    Implemented the dark mode styling

--- app/create/_components/RecipeCard.tsx
+++ app/create/_components/RecipeCard.tsx
@@ -15,13 +15,13 @@ export interface Recipe {
 
 const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
   return (
-    <Card className="p-5 bg-primary-100 border-primary-300">
+    <Card className="p-5 bg-primary-100 dark:bg-zinc-900 border-primary-300 dark:border-primary-800">
       <CardTitle className="text-lg">{recipe.name}</CardTitle>
       <CardDescription>{recipe.description}</CardDescription>
       <CardContent className="p-0">
         {/*Ingredients*/}
         <p className="mt-5 font-semibold text-primary-800">Ingredients:</p>
-        <div className="p-3 mt-3 bg-primary-200 rounded-lg">
+        <div className="p-3 mt-3 bg-primary-200 dark:bg-zinc-950 rounded-lg">
           {Object.entries(recipe.ingredients).map(([ingredient, amount]) => (
             <div className="flex items-center gap-2 my-1" key={ingredient}>
               <CookingPot size={16} strokeWidth={0.5} />
